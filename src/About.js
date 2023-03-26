63b877649238f1ef34dc2625ffefc10cb64766a3
Rawllings Kiprop <rutojr01@gmail.com>
2023-03-26 22:43:39 +0300

    Done with Log in, Sign Up and About us page

---
+++ src/About.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="">
+      <div className="text-center py-5 ">
+        <h1 className="text-5xl font-bold">About us</h1>
+      </div>
+      <div className="px-20 leading-8 ">
+        <p className="pt-10">
+          Ready to discover the best of Kenya? Look no further than our travel
+          website! Our platform offers a diverse range of top destinations
+          across Kenya, from the savannahs of the Maasai Mara to the white sandy
+          beaches of the coast. Our team of local experts has curated a
+          collection of must-see locations, complete with stunning photos,
+          insider tips, and detailed information to help you plan the ultimate
+          adventure. Whether you're looking to witness the Great Wildebeest
+          Migration, climb Mount Kenya, or soak up the sun in Malindi, our
+          website has everything you need to make the most of your trip. With
+          easy-to-use search and booking tools, personalized recommendations,
+          and reliable customer support, our website is your go-to resource for
+          planning an unforgettable Kenyan getaway. Visit us today and start
+          exploring the magic of Kenya!"
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
