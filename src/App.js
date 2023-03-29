ad1e68fe3b9856b90c639256a367936fa916325f
Rawllings Kiprop <rutojr01@gmail.com>
2023-03-29 11:25:03 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import About from "./components/About";
 import "./index.css";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import DestinationCard from './components/DestinationCard';
+// import DestinationCard from './components/DestinationCard';
 import Destinations from './components/Destinations';
 
 function App() {
