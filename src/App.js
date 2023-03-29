c327ed9fb547be8071e39428791949412dc68760
Francisca <117682018+Francieorwa@users.noreply.github.com>
2023-03-29 11:15:59 +0300

    updated files

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ import About from "./components/About";
 import "./index.css";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
 
     <div>
-       <Navbar />
-       <Section />
+      <Navbar />
+      <Section />
       <About />
       <Login />
       <SignUp />
